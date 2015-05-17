aaa0d84aa5002c70e63aad33fa65501492f734b9
bradyJ89 <omfguess@aim.com>
2015-05-16 22:51:14 -0400

    fb ui

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -268,4 +268,17 @@ angular.module('pokomonApp')
         });
     }
 
+    $scope.fbShare = function fbShare(){
+        var shareConfig = {
+            method: "feed",
+            name: "pokomon",
+            link: "http://pokomon.herokuapp.com",
+            href: "http://pokomon.herokuapp.com",
+            caption: "I just found this cool thing on pokogram!"
+        }
+        FB.ui(shareConfig, function(res){
+            console.log(res);
+        });
+    };
+
   });
