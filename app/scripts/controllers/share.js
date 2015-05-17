c415900d4634287ace72f86f1bb2b227d807d6b8
bradyJ89 <omfguess@aim.com>
2015-05-16 21:57:13 -0400

    fb ui

---
+++ app/scripts/controllers/share.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pokomonApp.controller:SharectrlCtrl
+ * @description
+ * # SharectrlCtrl
+ * Controller of the pokomonApp
+ */
+angular.module('pokomonApp')
+  .controller('shareCtrl', function ($scope) {
+    
+
+    $scope.fbShare = function fbShare(){
+    	var shareConfig = {
+    		method: "share",
+    		name: "pokomon",
+    		href: "pokomon.herokuapp.com",
+    		description: "I just found this cool thing on pokogram!"
+    	}
+    	FB.ui(shareConfig);
+    };
+
+  });
